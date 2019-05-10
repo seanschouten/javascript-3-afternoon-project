e358621d6a9c3ddfb06fff6cd1d251c43d1fa3dc
Sean Schouten <seanschouten@sean-schoutens-macbook.local>
2019-05-10 15:20:11 -0600

    Completed Higher Order

--- higher-order.js
+++ higher-order.js
@@ -21,7 +21,11 @@ const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
 */
 
 //Code Here
-let evenNumbers // = mixedNumbers.filter(/* Provide Your Callback Here */)
+let evenNumbers  = mixedNumbers.filter(function(
+  elem){
+    return elem % 2 ===0
+  }
+)
 
 
 
@@ -44,7 +48,9 @@ const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
 */
 
 //Code Here
-let postTaxPrices // = prices.map(/* Provide Your Callback Here );
+let postTaxPrices = prices.map(function(number)
+{return number * 1.07}
+) ;
 
 
 
@@ -63,7 +69,9 @@ const populations = [8175133, 3792621, 2695598, 2100263];
 */
 
 //Code Here
-let totalPopulation //  = populations.reduce(/* Provide Your Callback Here */)
+let totalPopulation  = populations.reduce(function ( acc, elem) {
+  return acc + elem
+}, 0)
 
 
 
@@ -89,7 +97,9 @@ const monstersInYourPocket = [{"monster":"Bulbabunny","CP":156},{"monster":"Bulb
 */
 
 //Code Here
-let myStrongest // = monstersInYourPocket.filter(/* Provide Your Callback Here */)
+let myStrongest = monstersInYourPocket.filter(function (monster){
+  return monster.CP > 200
+})
 
 
 
@@ -106,7 +116,11 @@ const orders = [{"price":15,"tax":0.09},{"price":42,"tax":0.07},{"price":56,"tax
   Use a higher order method to get all the order totals after adding in the sales tax. Your answer should be an array of numbers, one total for each order.
 */
 
-let orderTotals // Code here
+let orderTotals = orders.map (function (elem){
+  let {price, tax} = elem
+  return price * (1 + tax);
+})
+  // Code here
 
 
 
@@ -126,6 +140,14 @@ const purchases = [{"owner":"Barry","price":103},{"owner":"Bob","price":75},
   Use a high order method to create to get the sum of bobsTotal.
 */
 
-let bobsTotal //Code Here
+let bobsTotal = purchases.filter (function (tab){
+    return (tab.owner === "Bob")
+ }).map(function(elem){
+   let {price} = elem
+   return price
+ }).reduce(function(acc, elem2){
+    return acc + elem2
+}) //Code Here
 
 
+bobsTotal 
