8f678356742f7042c08b0667698ca11ad994d720
Sean Schouten <seanschouten@sean-schoutens-macbook.local>
2019-05-10 14:06:03 -0600

    Completed Spread

--- spread.js
+++ spread.js
@@ -19,7 +19,9 @@ let product = {
 
 // code here
 
-
+let productCopy = {
+  ... product
+}
 
 
 ////////// PROBLEM 2 //////////
@@ -30,7 +32,9 @@ let colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 
 // code here
 
-
+let colorsCopy = [
+  ...colors
+]
 
 
 ////////// PROBLEM 3 //////////
@@ -44,7 +48,7 @@ function addNums(num1, num2, num3) {
 }
 
 // code here
-
+let result = addNums(...numbers)
 
 ////////// PROBLEM 4 //////////
 
@@ -54,3 +58,4 @@ let arr1 = ['dog', 'chameleon', 'monkey'];
 let arr2 = ['bear', 'tiger', 'liger'];
 
 // code here
+let combinedArrays = [...arr1, ...arr2];
